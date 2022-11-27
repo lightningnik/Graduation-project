302d3503a1d27b6f1d0830282ed484227115daa6
lightningnik <73239458+lightningnik@users.noreply.github.com>
2022-11-27 11:27:12 +0300

    Update Create tables.sql

--- SQL/Create tables.sql
+++ SQL/Create tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Students(
-Student_id int IDENTITY(1,1),
+Student_id int PRIMARY KEY IDENTITY(1,1),
 Surname varchar(50),
 Name varchar(50),
 Patronymic varchar(50),
@@ -9,7 +9,7 @@ password varchar(50)
 );
 
 CREATE TABLE Employee(
-Employee_id int IDENTITY(1,1),
+Employee_id int PRIMARY KEY IDENTITY(1,1),
 Surname varchar(50),
 Name varchar(50),
 Patronymic varchar(50),
@@ -18,31 +18,38 @@ phone_number varchar(50),
 password varchar(50)
 );
 
+CREATE TABLE Work_types(
+id_type int,
+name_type int
+);
+
 CREATE TABLE Marks(
-Mark_id int IDENTITY(1,1),
+Mark_id int PRIMARY KEY IDENTITY(1,1),
 Mark int,
-Student_id int,
-Lesson_id int
+Student_id int FOREIGN KEY REFERENCES Students(Student_id),
+Lesson_id int FOREIGN KEY REFERENCES Lessons(Lesson_id),
+id_type int FOREIGN KEY REFERENCES Work_types(id_type)
 );
 
 CREATE TABLE Disciplines(
-Discipline_id int IDENTITY(1,1),
+Discipline_id int PRIMARY KEY IDENTITY(1,1),
 Discipline_name varchar(50)
 );
 
 CREATE TABLE Lessons(
-Lesson_id int IDENTITY(1,1),
+Lesson_id int PRIMARY KEY IDENTITY(1,1),
 Employee_id int,
 Discipline_id int,
 Group_id int,
 );
 
 CREATE TABLE Groups(
-Group_id int IDENTITY(1,1),
+Group_id int PRIMARY KEY IDENTITY(1,1),
 Group_name varchar(50)
 );
 
 CREATE TABLE Group_List(
 Student_id int,
-Group_id int,
+Group_id int FOREIGN KEY REFERENCES Groups(Group_id)
 );
+
